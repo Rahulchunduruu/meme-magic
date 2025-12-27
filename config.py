5aca308f9c7d3471808f598edf087b1b971a38fa
Rahul <rahulchunduruu@gmail.com>
2025-12-27 08:53:50 +0530

    Comment out env_path variable in config.py
    Comment out the env_path variable for clarity.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # Load .env from parent directory
-env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+#env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv()
 
 class Config:    
