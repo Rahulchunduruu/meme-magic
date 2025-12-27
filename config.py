8be2ba9af639d672b8e6294fba4cb5e92df077d7
Rahulchunduruu <rahulchunduruu@gmail.com>
2025-12-28 03:12:53 +0530

    removing uncessary api

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-# Load .env from parent directory
-#env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv()
 
 class Config:    
-    GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
-    GEMINI_API_KEY1=os.getenv('GEMINI_API_KEY1')
-    GEMINI_API_KEY2=os.getenv('GEMINI_API_KEY2')
-    GEMINI_API_KEY3=os.getenv('GEMINI_API_KEY3')
-    GEMINI_API_KEY4=os.getenv('GEMINI_API_KEY4')
+    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+    
+    if not GEMINI_API_KEY:
+        raise ValueError("GEMINI_API_KEY not found in .env file. Please add your API key.")
