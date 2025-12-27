3964f763d6a6a023c24a11f9040a7cec96454a44
Rahul <rahulchunduruu@gmail.com>
2025-12-28 03:51:47 +0530

    Clarify terminal execution instructions in config.py
    Added comment to clarify how to run the configuration.

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:    
+    #If you prefer to run this in the terminal, make sure the first line is active and the second is commented out
     #GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
     GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
     if not GEMINI_API_KEY:
